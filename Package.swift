496be1139c8479d67c6d480faa0589d95d67cf28
Lightspark Eng <engineering@lightspark.com>
2023-05-18 18:28:26 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 72237d7833326ac6c58e5f21587f4aff745bee61

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,6 +7,7 @@ let package = Package(
     name: "LightsparkWallet",
     platforms: [
         .iOS(.v13),
+        .macOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
