adaefca6521c7d213d062cbbd368f35cf009bc90
Zhen Lu <zhenlu@lightspark.com>
2023-05-24 12:33:49 -0700

    Improve JWTAuthManager - Add publisher and async methods. - Refactor some parameter.

--- Sources/LightsparkWallet/Auth/JWTAuthManager.swift
+++ Sources/LightsparkWallet/Auth/JWTAuthManager.swift
@@ -15,15 +15,56 @@ struct AccessToken: Codable {
 }
 
 public class JWTAuthManager {
-    public init(authStateStorage: AuthStateStorage = UserDefaultAuthStorage()) {
+    public enum JWTAuthManagerError: Error {
+        case parseError
+    }
+    public init(
+        authStateStorage: AuthStateStorage = UserDefaultAuthStorage(),
+        baseURLString: String? = nil,
+        additionalHttpHeaders: [AnyHashable: Any] = [:]
+    ) {
         self.authStateStorage = authStateStorage
+
+        var headers = additionalHttpHeaders
+        headers["User-Agent"] = Requester.userAgentString
+        headers["X-Lightspark-SDK"] = Requester.userAgentString
+        let config = URLSessionConfiguration.default
+        config.httpAdditionalHeaders = headers
+        self.urlSession = URLSession(configuration: config)
+        self.baseURLString = baseURLString
+    }
+
+    public func loginPublisher(
+        accountID: String,
+        secret: String
+    ) -> AnyPublisher<String, Error> {
+        let variables = [
+            "account_id": accountID,
+            "jwt": secret,
+        ]
+
+        let request: URLRequest
+        do {
+            request = try URLRequest.buildURLRequest(
+                baseURLString: self.baseURLString,
+                operation: self.jwtMutation,
+                variables: variables,
+                signingKey: nil
+            )
+        } catch {
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+
+        return self.urlSession.dataTaskPublisher(for: request)
+            .tryMap { data, _ in
+                try self.parseResultDataToAccessToken(data: data)
+            }
+            .eraseToAnyPublisher()
     }
 
     public func login(
         accountID: String,
         secret: String,
-        baseURLString: String? = nil,
-        additionalHttpHeaders: [AnyHashable: Any] = [:],
         completion: @escaping (String?, Error?) -> Void
     ) {
         let variables = [
@@ -34,7 +75,7 @@ public class JWTAuthManager {
         let request: URLRequest
         do {
             request = try URLRequest.buildURLRequest(
-                baseURLString: baseURLString,
+                baseURLString: self.baseURLString,
                 operation: self.jwtMutation,
                 variables: variables,
                 signingKey: nil
@@ -44,38 +85,35 @@ public class JWTAuthManager {
             return
         }
 
-        var headers = additionalHttpHeaders
-        headers["User-Agent"] = Requester.userAgentString
-        headers["X-Lightspark-SDK"] = Requester.userAgentString
-
-        let config = URLSessionConfiguration.default
-        config.httpAdditionalHeaders = headers
-        let urlSession = URLSession(configuration: config)
-
-        urlSession.dataTask(with: request) { data, response, error in
+        self.urlSession.dataTask(with: request) { data, response, error in
             guard let data = data else {
                 completion(nil, nil)
                 return
             }
             do {
-                let response = try JSONDecoder.lightsparkJSONDecoder().decode(
-                    Response<LoginWithJWTOutput>.self,
-                    from: data
-                )
-                if let data = response.data {
-                    let output = data
-                    let token = AccessToken(token: output.accessToken, expiration: output.validUntil)
-                    let tokenData = try JSONEncoder().encode(token)
-                    self.authStateStorage.storeAuthStateData(data: tokenData)
-                    completion(token.token, nil)
-                } else {
-                    completion(nil, response.errors?.first)
-                }
+                completion(try self.parseResultDataToAccessToken(data: data), nil)
             } catch {
                 completion(nil, error)
             }
         }
         .resume()
+    }
+
+    public func login(accountID: String, secret: String) async throws -> String {
+        let variables = [
+            "account_id": accountID,
+            "jwt": secret,
+        ]
+
+        let request = try URLRequest.buildURLRequest(
+            baseURLString: self.baseURLString,
+            operation: self.jwtMutation,
+            variables: variables,
+            signingKey: nil
+        )
+
+        let (data, _) = try await self.urlSession.data(for: request)
+        return try self.parseResultDataToAccessToken(data: data)
     }
 
     public func loadAuthToken() -> String? {
@@ -92,7 +130,24 @@ public class JWTAuthManager {
         return token.token
     }
 
+    private func parseResultDataToAccessToken(data: Data) throws -> String {
+        let response = try JSONDecoder.lightsparkJSONDecoder().decode(
+            Response<LoginWithJWTOutput>.self,
+            from: data
+        )
+        guard let data = response.data else {
+            throw JWTAuthManagerError.parseError
+        }
+
+        let token = AccessToken(token: data.accessToken, expiration: data.validUntil)
+        let tokenData = try JSONEncoder().encode(token)
+        self.authStateStorage.storeAuthStateData(data: tokenData)
+        return token.token
+    }
+
     private let authStateStorage: AuthStateStorage
+    private let urlSession: URLSession
+    private let baseURLString: String?
 
     private let jwtMutation = """
         mutation LogingWithJWT(
